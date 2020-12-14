65bccff6c67f0114bf34636c21cb9efb3eae24de
MallikaPanjwani <malli.panj@gmail.com>
2020-12-14 14:12:29 +0530

    Reactive forms added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,33 @@
-import { Component } from "@angular/core";
-import { NgForm } from "@angular/forms";
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, NgForm } from "@angular/forms";
 
 @Component({
   selector: "my-app",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  myForm: FormGroup;
   userName = "";
   email: string;
-  nickName: string;
   password: string;
 
+  ngOnInit() {
+    this.myForm = new FormGroup({
+      name: new FormControl(""),
+      emailRf: new FormControl(""),
+      passwordRf: new FormControl("")
+    });
+  }
   onSubmit(form: NgForm) {
     if (form.valid) {
       console.log(form.value);
     }
+  }
+  onSubmitRf(form: FormGroup) {
+    console.log("Valid?", form.valid);
+    console.log("Name", form.value.name);
+    console.log("Email", form.value.emailRf);
+    console.log("Password", form.value.passwordRf);
   }
 }
